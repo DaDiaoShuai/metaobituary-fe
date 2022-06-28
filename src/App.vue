8ba8f0e95ad068dd271e92156b7e1cecc591f9c0
coolb <jingshuai7687@gmail.com>
2022-06-28 22:12:30 +0800

    feat: rm suspense

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,12 @@
 import { RouterLink, RouterView } from "vue-router";
 import HeadComp from "./components/HeadComp.vue";
 import FooterComp from "./components/FooterComp.vue";
-import { Suspense } from "vue";
 </script>
 
 <template>
 	<div id="appMain" class="container pt-14 px-3 flex-1 max-w-7xl mx-auto">
 		<HeadComp />
-		<Suspense>
-			<RouterView />
-			<template #fallback> Loading... </template>
-		</Suspense>
+		<RouterView />
 	</div>
 	<FooterComp />
 </template>
